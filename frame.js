feab5346472b688b87320482e7d4494865ec83c9
Oblyv <gregrmcbride@gmail.com>
2026-01-26 14:11:19 -0500

    Create frame.js

---
+++ frame.js
@@ -0,0 +1,30 @@
+const navHTML = `
+<div class="nav-inner">
+  <img src="/res/orbit-no-bg.png" class="nav-logo">
+  <div class="nav-links">
+    <a href="/"><i class="fa-solid fa-house"></i> Home</a>
+    <a href="/g"><i class="fa-solid fa-gamepad"></i> Games</a>
+    <a href="/v"><i class="fa-solid fa-video"></i> Videos</a>
+    <a href="/s"><i class="fa-solid fa-gear"></i> Settings</a>
+  </div>
+  <i class="fa-solid fa-bars menu-btn" id="menuBtn"></i>
+</div>
+`
+
+const menuHTML = `
+<a href="/"><i class="fa-solid fa-house"></i> Home</a>
+<a href="/g"><i class="fa-solid fa-gamepad"></i> Games</a>
+<a href="/v"><i class="fa-solid fa-video"></i> Videos</a>
+<a href="/s"><i class="fa-solid fa-gear"></i> Settings</a>
+`
+
+const navMount = document.getElementById("navMount")
+const menu = document.getElementById("menu")
+
+if (navMount) navMount.innerHTML = navHTML
+if (menu) menu.innerHTML = menuHTML
+
+const menuBtn = document.getElementById("menuBtn")
+if (menuBtn && menu) {
+  menuBtn.onclick = () => menu.classList.toggle("open")
+}

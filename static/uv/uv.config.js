eb9152e73c041486807bc0e2cdc9d68211c6a3dd
Oblyv <gregrmcbride@gmail.com>
2026-02-12 17:30:38 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 
 self.__uv$config = {
-	prefix: "/static/petezah/",
+	prefix: "/static/orbit/",
 	bare: "/api/youtube-bypass/bare/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,

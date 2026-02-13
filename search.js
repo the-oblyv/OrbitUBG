12d870c83a0cfddf564a363894b58984ec39b482
Oblyv <gregrmcbride@gmail.com>
2026-02-13 17:25:57 -0500

    Create search.js

---
+++ search.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+*
+* @param {string} input
+* @param {string} template
+* @returns {string}
+*/
+function search(input, template) {
+	try {
+		return new URL(input).toString();
+	} catch (err) {
+	}
+	try {
+		const url = new URL(`http://${input}`);
+		if (url.hostname.includes(".")) return url.toString();
+	} catch (err) {
+	}
+	return template.replace("%s", encodeURIComponent(input));
+}

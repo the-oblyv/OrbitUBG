0faef8673720c2e209a28e1b807de3d1a7044be2
Oblyv <gregrmcbride@gmail.com>
2026-02-13 13:58:24 -0500

    Update frame.js

--- frame.js
+++ frame.js
@@ -5,6 +5,7 @@ const navHTML = `
     <a href="/"><i class="fa-solid fa-house"></i> Home</a>
     <a href="/g"><i class="fa-solid fa-gamepad"></i> Games</a>
     <a href="/a"><i class="fa-solid fa-archive"></i> Apps</a>
+    <a href="/ai"><i class="fa-solid fa-robot"></i> AI</a>
     <a href="/s"><i class="fa-solid fa-gear"></i> Settings</a>
   </div>
   <i class="fa-solid fa-bars menu-btn" id="menuBtn"></i>
@@ -15,6 +16,7 @@ const menuHTML = `
 <a href="/"><i class="fa-solid fa-house"></i> Home</a>
 <a href="/g"><i class="fa-solid fa-gamepad"></i> Games</a>
 <a href="/a"><i class="fa-solid fa-archive"></i> Apps</a>
+<a href="/ai"><i class="fa-solid fa-robot"></i> AI</a>
 <a href="/s"><i class="fa-solid fa-gear"></i> Settings</a>
 `
 

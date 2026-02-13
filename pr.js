44473e024e891c2535406f07109845729dbb8c75
Oblyv <gregrmcbride@gmail.com>
2026-02-13 16:30:32 -0500

    Update pr.js

--- pr.js
+++ pr.js
@@ -1,11 +1,10 @@
-const proxyBase = "https://3.dmvdriverseducation.org/vpr/go/";
+const proxyBase = "https://3.dmvdriverseducation.org/vpr/go/"; 
 const iframe = document.getElementById("proxyFrame");
 
 function getTargetUrl() {
-    const params = new URLSearchParams(window.location.search);
     const query = window.location.href.split("?")[1];
-    if (query) return query;
-    return "https://start.duckduckgo.com/";
+    if (query) return encodeURIComponent(query);
+    return encodeURIComponent("https://start.duckduckgo.com/");
 }
 
 iframe.src = proxyBase + getTargetUrl();

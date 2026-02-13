2baed7948d2ab27482ac42b389b16cf120038daa
Oblyv <gregrmcbride@gmail.com>
2026-02-13 17:42:32 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-let host = location.protocol + '//' + location.host;
-
-let _CONFIG = {
-  wispurl: localStorage.getItem('proxServer') || (location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + '/wisp/',
-  bareurl: host + '/bare/'
-};

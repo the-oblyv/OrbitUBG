274313c2dc3cd6c6c9b7721a78931af761855e79
Oblyv <gregrmcbride@gmail.com>
2026-03-01 12:04:25 -0500

    Update frame.js

--- frame.js
+++ frame.js
@@ -13,7 +13,7 @@ const navHTML = `
 <i class="fa-solid fa-ellipsis"></i> More
 </button>
 <div class="more-dropdown">
-<a href="/partners"><img src="/res/partners.svg" class="partner-card"></img> Partners</a>
+<a href="/partners"><img src="/res/partners.svg" class="partner-img"></img> Partners</a>
 <a href="/s"><i class="fa-solid fa-gear"></i> Settings</a>
 </div>
 </div>
@@ -29,7 +29,7 @@ const menuHTML = `
 <a href="/med"><i class="fa-solid fa-photo-film"></i> Media</a>
 <a href="/prx"><i class="fa-solid fa-globe"></i> Proxy</a>
 <hr style="border:none;height:1px;background:rgba(255,255,255,.1);margin:10px 0;">
-<a href="/partners"><img src="/res/partners.svg" class="partner-card"></img> Partners</a>
+<a href="/partners"><img src="/res/partners.svg" class="partner-img"></img> Partners</a>
 <a href="/s"><i class="fa-solid fa-gear"></i> Settings</a>
 `;
 
